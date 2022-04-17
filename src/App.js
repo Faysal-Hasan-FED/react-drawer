ff6284338b9a78af9b30ac226f9e9b91edbd00e4
Faysal <frontendfaysal@gmail.com>
2022-04-18 01:52:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { HashRouter } from 'react-router-dom';
+import { BrowserRouter,Route ,Routes} from 'react-router-dom';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Sidebar></Sidebar>
+    <HashRouter>
+      <Route path="" element={<Home></Home>}></Route>
+      <Route path="/home" element={<Home></Home>}></Route>
+      <Route path="/products" element={<Product></Product>}></Route>
+    </HashRouter>
+    </BrowserRouter>
+    
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    console.log('home');
+    return (
+        <div className='home'>
+            <h2>This is home</h2>
+            
+        </div>
+    );
+};
+
+export default Home;
